a5e4ebee59d48c2db33d9c503a4f1b1deae64a51
Varun Prakash <varunpatnabh@gmail.com>
2026-04-27 19:19:08 +0000

    new chqnges

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Currency Converter Bot is running 🚀"
+
+@app.route("/convert", methods=["GET"])
+def convert_currency():
+    from_currency = request.args.get("from")
+    to_currency = request.args.get("to")
+    amount = request.args.get("amount")
+
+    if not from_currency or not to_currency or not amount:
+        return jsonify({"error": "Missing parameters"}), 400
+
+    url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+    data = requests.get(url).json()
+
+    return jsonify({
+        "from": from_currency,
+        "to": to_currency,
+        "amount": amount,
+        "converted_amount": data.get("result")
+    })
