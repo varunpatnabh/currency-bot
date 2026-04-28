567fb168deb7b22b892c5b54500ea8bbb85f8501
Varun Prakash <varunpatnabh@gmail.com>
2026-04-28 10:39:35 +0530

    Refactor currency conversion endpoint
    Updated currency conversion logic to use a new API and handle errors.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def home():
     return "App is running 🚀", 200
 
+
 @app.route("/convert", methods=["GET"])
 def convert_currency():
     from_currency = request.args.get("from")
@@ -17,14 +18,24 @@ def convert_currency():
     if not from_currency or not to_currency or not amount:
         return jsonify({"error": "Missing parameters"}), 400
 
-    url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}&amount={amount}"
+    url = f"https://open.er-api.com/v6/latest/{from_currency}"
     data = requests.get(url).json()
+
+    if data.get("result") != "success":
+        return jsonify({"error": "API failed"}), 500
+
+    rate = data["rates"].get(to_currency)
+
+    if not rate:
+        return jsonify({"error": "Invalid currency"}), 400
+
+    converted = float(amount) * rate
 
     return jsonify({
         "from": from_currency,
         "to": to_currency,
         "amount": amount,
-        "converted_amount": data.get("result")
+        "converted_amount": round(converted, 2)
     })
 
 if __name__ == "__main__":
